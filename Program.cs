baa7f3b6f38d9215bc66d1f623b7e255bcf5cdde
cstpalash <cstpalash@gmail.com>
2021-12-13 14:48:12 +0530

    Day13

--- Program.cs
+++ Program.cs
@@ -13,9 +13,11 @@ namespace AOC._2021
             Console.WriteLine("AoC 2021");
             Console.WriteLine("=======================================================");
 
+            //Dec14();
+
             Dec13();
-            //Dec12();
             /*
+            Dec12();
             Dec11();
             Dec10();
             Dec9();
@@ -30,6 +32,18 @@ namespace AOC._2021
             */
 
             Console.ReadLine();
+        }
+
+        private static void Dec14()
+        {
+            var date = new Dec14();
+
+            var lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Input/Dec14.txt"));
+
+
+            Console.WriteLine(string.Format("Dec14 Puzzle1 Result : {0}", date.Puzzle1(lines)));
+            Console.WriteLine(string.Format("Dec14 Puzzle2 Result : {0}", date.Puzzle2(lines)));
+            Console.WriteLine("=======================================================");
         }
 
         private static void Dec13()
